73d7638e43833f60a319365ec203fc9626a1be7b
TMVinicius <viniciustavaresbr@gmail.com>
2025-12-16 16:46:37 -0300

    criando o domain Anime

---
+++ src/main/java/com/tmvinicius/devdojo2/domain/Anime.java
@@ -0,0 +1,13 @@
+package com.tmvinicius.devdojo2.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Anime {
+
+    private Long id;
+    private String name;
+
+}
