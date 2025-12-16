016b0cb312ef99ba141291ab39142ac84db59368
TMVinicius <viniciustavaresbr@gmail.com>
2025-12-16 16:42:52 -0300

    Criando estrutura do projeto

---
+++ src/test/java/com/tmvinicius/devdojo2/DevDojo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmvinicius.devdojo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevDojo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
