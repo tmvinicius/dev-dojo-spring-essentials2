d136709b279ce7acb4e7141f5f2eb813d580a973
TMVinicius <viniciustavaresbr@gmail.com>
2025-12-16 16:52:38 -0300

    intergace a ser utilizada para implementar os metodos que servirao o Service da applicacao

---
+++ src/main/java/com/tmvinicius/devdojo2/repository/AnimeRepository.java
@@ -0,0 +1,10 @@
+package com.tmvinicius.devdojo2.repository;
+
+import com.tmvinicius.devdojo2.domain.Anime;
+
+import java.util.List;
+
+public interface AnimeRepository {
+
+    List<Anime> listAll();
+}
